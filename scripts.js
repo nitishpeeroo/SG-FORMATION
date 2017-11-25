d4d1961870d1d46feb044f93c7f33c120fc113e9
alexisjlv <alexis.jolivel@gmail.com>
2017-11-25 17:13:00 +0100

    page search + page sequence

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/**
+ *  Affichage de la popup
+ */
+
+$("#search_mod-menu").click(function () {
+    $("#search_mod").addClass("opened");
+});
+$(".search_mod-close").click(function () {
+    $("#search_mod").removeClass("opened");
+});
