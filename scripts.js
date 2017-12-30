c819412f52ddb2ea01c85b579475f361c6e75e47
alexisjlv <alexis.jolivel@gmail.com>
2017-12-30 22:36:14 +0100

    corrections

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
-/**
- *  Affichage de la popup
- */
 
-$("#search_mod-menu").click(function () {
-    $("#search_mod").addClass("opened");
+$(document).ready(function () {
+    /**
+     *  Affichage de la popup
+     */
+    $("#search_mod-menu").click(function () {
+        $("#search_mod").addClass("opened");
+    });
+    $(".search_mod-close").click(function () {
+        $("#search_mod").removeClass("opened");
+    });
+
+
+    /**
+     * clic bouton video
+     */
+    $("#toVideo").click(function() {
+        console.log("test");
+        $('html, body').animate({
+            scrollTop: $("#video").offset().top - 50
+        }, 700);
+    });
 });
-$(".search_mod-close").click(function () {
-    $("#search_mod").removeClass("opened");
-});
-
-
-
-
-
-
-
